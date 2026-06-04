329d53b2cf44ff148b1be44eb4ad6fe36b7ced12
Poornima M <poornimamurali2803@gmail.com>
2026-06-04 16:45:42 +0530

    create multiple pages using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { HomePage } from './pages/HomePage'
 import './App.css'
+import { Route, Routes } from 'react-router'
 
 function App() {
 
   return (
-   <HomePage />
+    <Routes >
+      <Route index element={ <HomePage />} />
+      <Route path = 'checkout' element={ <HomePage />} />
+
+    </Routes>
+  
   )
 }
 
