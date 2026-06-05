0b5f52b7a95c954cad7941c0e30951899f94e062
Poornima M <poornimamurali2803@gmail.com>
2026-06-05 16:14:09 +0530

    updated home page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+import './NotFoundPage.css'
+export function NotFoundPage() {
+    return(
+       <>
+            <Header/>
+            <div className="notfound-text">
+                Page Not found!!
+            </div>
+       </>
+    )
+}
